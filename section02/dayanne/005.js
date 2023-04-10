741663afa16ea5c4f2261fb354f4b760a42e318f
Da-Youn <dayoun202@gmail.com>
2023-04-11 01:24:00 +0900

    Add: section02 005번 풀이 설명 [dayanne]

--- section02/dayanne/005.js
+++ section02/dayanne/005.js
@@ -10,15 +10,16 @@ function solution(n, scores) {
   const rank = new Array(n).fill(1);
   const copyScores = scores.slice();
 
-  copyScores.sort((a, b) => b - a);
+  copyScores.sort((a, b) => b - a); // 복사한 배열을 내림차순 정렬 [100, 92, 89, 87, 76]
 
   for (let i = 0; i < n; i++) {
-    const index = copyScores.indexOf(scores[i]);
+    const index = copyScores.indexOf(scores[i]); // scores 값이 내림차순 정렬된 상태의 위치(순위) 기억 (87 = 3, 89 = 2, 92 = 1, 100 = 0, 76 = 4)
 
     if (index > 0 && copyScores[index - 1] === scores[i]) {
-      rank[i] = rank[i - 1];
+      // 이전 인덱스의 성적 값과 동일할 때
+      rank[i] = rank[i - 1]; // 순위를 그대로 유지
     } else {
-      rank[i] += index;
+      rank[i] += index; // index 값 더하기(현재 순위를 가져옴)
     }
   }
 
