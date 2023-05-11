36882561a70af6c77ad327fcb04f93c855666f7c
d-charlie-kim <d.charlie.kim@gmail.com>
2023-05-11 22:42:07 +0900

    [Solved] Charlie Article 004 solved <bruteForce>

--- article02/charlie/005.js
+++ article02/charlie/005.js
@@ -14,7 +14,68 @@ word는 알파벳 대문자 'A', 'E', 'I', 'O', 'U'로만 이루어져 있습니
 */
 
 function solution(n) {
-    
-	var answer = 0;
-	return answer;
+	let count = 1;
+	const compare = ['A'];
+	const alpha = ['A', 'E', 'I', 'O', 'U'];
+
+	// 같은 것은 반드시 나오니, 계속 반복한다.
+	while (true) {
+		if (n === compare.join(''))  // 찾았다! 여기까지~ 해산!
+			return count;
+
+		if (compare.length < 5) {	// A다음은 AA, AE 다음도 AEA, 길이가 5미만이면 반드시 뒤에 A가 추가된다.
+			compare.push('A');
+		}
+		else if (compare[compare.length - 1] === 'U') {
+			while(compare[compare.length - 1] === 'U')  // 가장 오른쪽이 U면 U는 빼준다. // AAAAU -> AAAA || AAUUU -> AA
+				compare.pop();
+			
+			compare.push(alpha[alpha.indexOf(compare.pop()) + 1]);  // 그 뒤에 끝을 바꿔준다. // AAAA -> AAAE || AA -> AE
+		}
+		else {
+			compare.push(alpha[alpha.indexOf(compare.pop()) + 1]);  // 그 외의 경우는 끝만 바꿔준다. // AAAA -> AAAE || AA -> AE
+		}
+		count++;
+	}
 }
+
+// 끝만 바꿔주는 방식은 pop() 한 것을 alpha 배열에서 index 찾고 그 다음 index의 값을 push() 해주는 것. 
+
+console.log(solution('A')); // 1
+console.log(solution('AA')); // 2
+console.log(solution('AAAAE')); // 6
+console.log(solution('AAAAU')); // 9
+console.log(solution('AAAE')); // 10
+console.log(solution('I')); // 1563
+console.log(solution('EIO')); //1189
+console.log(solution('UUUUU')); //3905
+
+/*
+
+A
+AA
+AAA
+AAAA
+AAAAA
+AAAAE
+AAAAI
+AAAAO
+AAAAU
+AAAE
+AAAEA
+AAAEE
+...
+AUUUU
+E
+EA
+EAA
+EAAA
+EAAAA
+EAAAE
+EAAAI
+EAAAO
+EAAAU
+EAAE
+EAAEA
+
+*/
