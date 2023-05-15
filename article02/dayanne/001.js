b539f11b9b3410ba91c1c7cac8bb8838fe386f6c
Da-Youn <dayoun202@gmail.com>
2023-05-15 14:44:44 +0900

    Fixed: article02 001 문제 및 풀이 수정

--- article02/dayanne/001.js
+++ article02/dayanne/001.js
@@ -1,33 +1,31 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/120848
+/*
+팩토리얼
+
+// 문제 설명
+i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다.
+예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다.
+정수 n이 n팩토리얼(n!)을 return 하도록 solution 함수를 완성해주세요.
+
+*/
 
 // 반복문
-function solution(n) {
+function solutionA(n) {
   let answer = 1;
-  let count = 0;
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 2; i <= n; i++) {
     answer *= i;
-    if (answer > n) {
-      break;
-    }
-    count++;
   }
-  return count;
+  return answer;
 }
 
-// 일반
+//재귀
 function solutionB(n) {
-  let num = 1;
-  let fac = 1;
-  function factorial(n, fac, num) {
-    if (fac > n) {
-      return num - 1;
-    }
-    return factorial(n, fac * (num + 1), num + 1);
+  if (n === 1) {
+    return 1;
   }
-  return factorial(n, fac, num);
+  return n * solutionB(n - 1);
 }
-const numA = 3628800;
-const numB = 7;
 
-console.log(solutionB(numA)); // 10
-console.log(solutionB(numB)); // 3
+console.log(solutionA(5)); // 120
+console.log(solutionB(5)); // 120
+console.log(solutionA(10)); // 3628800
+console.log(solutionB(10)); // 3628800
